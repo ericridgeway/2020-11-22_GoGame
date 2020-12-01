76d4938f3421d498fdadb86d436dd8b67d001a57
Eric Ridgeway <ericridgeway@gmail.com>
2020-11-30 23:06:14 -0500

    Not working try the usual liveview @assign directly in the phaser code

--- go/lib/go_web/live/game_phaser_live.ex
+++ go/lib/go_web/live/game_phaser_live.ex
@@ -3,12 +3,18 @@ defmodule GoWeb.GamePhaserLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, new(socket)}
   end
 
   @impl true
   def handle_event("decrement", values, socket) do
     IO.puts ""; require InspectVars; InspectVars.inspect([values])
-    {:noreply, socket}
+    {:noreply, socket |> assign(:live_todo, "new")}
+  end
+
+
+  defp new(socket) do
+    socket
+    |> assign(:live_todo, "asd123")
   end
 end

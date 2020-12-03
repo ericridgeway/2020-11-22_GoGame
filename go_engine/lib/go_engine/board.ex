bae0236f1d3ea35167af5023c2e759a41f8e41eb
Eric Ridgeway <ericridgeway@gmail.com>
2020-12-03 16:05:12 -0500

    Board struct

--- go_engine/lib/go_engine/board.ex
+++ go_engine/lib/go_engine/board.ex
@@ -1,16 +1,22 @@
 defmodule GoEngine.Board do
+  defstruct [:size, pieces: %{}]
 
   def new(size \\ 9) do
-    %{}
+    struct(__MODULE__, size: size)
   end
 
   def add_piece(t, color, x, y) do
-    Map.put(t, {x, y}, color)
+    # TODO Pieces.
+    t
+    |> pieces(Map.put(pieces(t), {x, y}, color))
   end
 
   def has_piece?(t, color, x, y) do
-    color == Map.get(t, {x, y})
+    color == Map.get(pieces(t), {x, y})
   end
 
-  def size(t), do: 9
+  def size(t), do: t.size
+
+  defp pieces(t), do: t.pieces
+  defp pieces(t, new), do: struct!(t, pieces: new)
 end

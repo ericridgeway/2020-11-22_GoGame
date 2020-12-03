c7e295da75d585c5bf6ea5ce100abd22120b1e5f
Eric Ridgeway <ericridgeway@gmail.com>
2020-12-03 15:35:51 -0500

    Starter tests

---
+++ go_engine/lib/go_engine/board.ex
@@ -0,0 +1,14 @@
+defmodule GoEngine.Board do
+
+  def new() do
+    -1
+  end
+
+  def add_piece(t, color, x, y) do
+    -1
+  end
+
+  def has_piece?(t, color, x, y) do
+    true
+  end
+end
---
+++ go_engine/test/board_test.exs
@@ -0,0 +1,15 @@
+defmodule GoEngineTest.Board do
+  use ExUnit.Case
+
+  alias GoEngine.{Board}
+
+  test "New board" do
+    board =
+      Board.new()
+      |> Board.add_piece(:black, 1, 1)
+
+    # TODO Alternative for has_piece? if only tests need it eventually?
+    assert Board.has_piece?(board, :black, 1, 1)
+    # refute Board.has_piece?(board, :white, 1, 1)
+  end
+end

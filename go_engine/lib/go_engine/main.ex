9bbdee2b18161d21ca9a9266d54d24f0d9f7a0de
Eric Ridgeway <ericridgeway@gmail.com>
2020-12-05 15:57:04 -0500

    Mostly re-adding what had before, starting in a "topmost" interface. I can put edge case checks right here and not feel bad. And can call everything from here hopefully, and it just splits off sub modules when enough pieces inside form a group???

--- go_engine/lib/go_engine/main.ex
+++ go_engine/lib/go_engine/main.ex
@@ -1,3 +1,32 @@
 defmodule GoEngine.Main do
+  defmodule Pieces do
+    defdelegate new(), to: Map
 
+    def add(t, color, x, y) do
+      Map.put(t, {x, y}, color)
+    end
+
+    def has_piece?(t, color, x, y) do
+      color == Map.get(t, {x, y})
+    end
+  end
+
+  defstruct [pieces: Pieces.new()]
+
+  def new() do
+    struct(__MODULE__)
+  end
+
+  def add_piece(t, color, x, y) do
+    t
+    |> update_pieces(Pieces.add(pieces(t), color, x, y))
+  end
+
+  def has_piece?(t, color, x, y) do
+    Pieces.has_piece?(pieces(t), color, x, y)
+  end
+
+
+  defp pieces(t), do: t.pieces
+  defp update_pieces(t, new), do: struct!(t, pieces: new)
 end
--- go_engine/test/main_test.exs
+++ go_engine/test/main_test.exs
@@ -2,4 +2,15 @@ defmodule GoEngineTest.Main do
   use ExUnit.Case
 
   alias GoEngine.{Main}
+
+  describe "Pieces" do
+    test "add_piece & has_piece?" do
+      main =
+        Main.new()
+        |> Main.add_piece(:black, 1, 1)
+
+      assert Main.has_piece?(main, :black, 1, 1)
+      refute Main.has_piece?(main, :white, 1, 1)
+    end
+  end
 end

e13b737f55d2021d98e76adc5246ceed9207e159
Eric Ridgeway <ericridgeway@gmail.com>
2020-12-05 22:31:46 -0500

    Try extracting Ascii module

---
+++ go_engine/lib/go_engine/ascii.ex
@@ -0,0 +1,25 @@
+defmodule GoEngine.Ascii do
+  alias GoEngine.Main
+
+  def get_from_main(main) do
+    Enum.reduce(1..Main.size(main), [], fn (row_num, row_list) ->
+      [columns(main, row_num) | row_list]
+    end)
+    |> Enum.reverse()
+  end
+
+
+  defp columns(main, row_num) do
+    Enum.reduce(1..Main.size(main), [], fn (col_num, col_list) ->
+      letter =
+        cond do
+          Main.has_piece?(main, :black, col_num, row_num) -> "b"
+          Main.has_piece?(main, :white, col_num, row_num) -> "w"
+          true -> "0"
+        end
+
+        [letter | col_list]
+    end)
+    |> Enum.reverse()
+  end
+end
--- go_engine/lib/go_engine/main.ex
+++ go_engine/lib/go_engine/main.ex
@@ -1,5 +1,5 @@
 defmodule GoEngine.Main do
-  alias GoEngine.{Pieces}
+  alias GoEngine.{Pieces, Ascii}
 
   defstruct [:size, pieces: Pieces.new()]
 
@@ -46,32 +46,13 @@ defmodule GoEngine.Main do
     Pieces.has_piece?(pieces(t), color, x, y)
   end
 
-  def ascii(t) do
-    Enum.reduce(1..size(t), [], fn (row_num, row_list) ->
-      [columns(t, row_num) | row_list]
-    end)
-    |> Enum.reverse()
-  end
+  def ascii(t), do: Ascii.get_from_main(t)
 
   def size(t), do: t.size
 
 
   defp pieces(t), do: t.pieces
   defp update_pieces(t, new), do: struct!(t, pieces: new)
-
-  defp columns(t, row_num) do
-    Enum.reduce(1..size(t), [], fn (col_num, col_list) ->
-      letter =
-        cond do
-          has_piece?(t, :black, col_num, row_num) -> "b"
-          has_piece?(t, :white, col_num, row_num) -> "w"
-          true -> "0"
-        end
-
-        [letter | col_list]
-    end)
-    |> Enum.reverse()
-  end
 
   defp check_x_and_y_lengths_match(list, target_size) do
     all_match = Enum.all?(list, fn row ->

441e0d8a5f7ea90b9e825e1607ca0d40e066f1c6
Eric Ridgeway <ericridgeway@gmail.com>
2020-12-09 18:47:13 -0500

    Split Ascii test

--- go_engine/test/main_test.exs
+++ go_engine/test/main_test.exs
@@ -32,62 +32,6 @@ defmodule GoEngineTest.Main do
     end
   end
 
-  describe "Ascii" do
-    test "Blank ascii" do
-      main =
-        Main.new(2)
-
-      assert Main.ascii(main) == [
-        ~w[0 0],
-        ~w[0 0],
-      ]
-    end
-
-    test "After adding pieces" do
-      main =
-        Main.new(3)
-        |> Main.add_piece(:black, 1, 1)
-        |> Main.add_piece(:white, 3, 2)
-
-      assert Main.ascii(main) == [
-        ~w[b 0 0],
-        ~w[0 0 w],
-        ~w[0 0 0],
-      ]
-    end
-
-    test "Generate main FROM ascii" do
-      target_main =
-        Main.new(3)
-        |> Main.add_piece(:black, 1, 1)
-        |> Main.add_piece(:white, 3, 2)
-        |> Main.add_piece(:white, 2, 3)
-
-      generated_main = [
-        ~w[b 0 0],
-        ~w[0 0 w],
-        ~w[0 w 0],
-      ]
-      |> Main.new_from_ascii()
-
-      assert generated_main == target_main
-    end
-
-    test "Error if row length doesnt match num cols" do
-      assert {:error, :width_and_height_must_match} = [
-        ~w[0 0],
-        ~w[0 0 0],
-      ]
-      |> Main.new_from_ascii()
-
-      assert {:error, :width_and_height_must_match} = [
-        ~w[w],
-        ~w[b b],
-      ]
-      |> Main.new_from_ascii()
-    end
-  end
-
   describe "Liberties" do
     test "single stone all liberties" do
       main = [

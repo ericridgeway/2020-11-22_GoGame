56a123a217a104c07e9a3ea45d8c7b20e24e2fbc
Eric Ridgeway <ericridgeway@gmail.com>
2020-12-10 18:13:27 -0500

    Filll out more of liveview interface

--- go/lib/go_web/live/game_svg_live.ex
+++ go/lib/go_web/live/game_svg_live.ex
@@ -8,6 +8,13 @@ defmodule GoWeb.GameSvgLive do
     {:ok, socket |> new()}
   end
 
+  def handle_event("add_piece", %{"x"=>x, "y"=>y}, socket) do
+    x = String.to_integer(x)
+    y = String.to_integer(y)
+
+    {:noreply, socket |> add_piece(x, y)}
+  end
+
   defp new(socket) do
     socket
     |> assign(:main,
@@ -15,5 +22,11 @@ defmodule GoWeb.GameSvgLive do
       |> Main.add_piece(:black, 1, 1)
       |> Main.add_piece(:white, 2, 2)
     )
+  end
+
+  defp add_piece(%{assigns: %{main: main}}=socket, x, y) do
+    # color = Main.current...
+    color = :black
+    assign(socket, main: Main.add_piece(main, color, x, y))
   end
 end
